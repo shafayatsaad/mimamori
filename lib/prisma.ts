e3e1f5d8a05796e5f78841fd8a60674d4f3088bb
Shafayat Saad <dev.saad30@gmail.com>
2026-05-30 21:03:26 +0600

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,11 @@ import { PrismaClient } from '@/app/generated/prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasourceUrl: process.env.POSTGRES_PRISMA_URL,
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
